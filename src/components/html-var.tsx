1705961d7d4dfe8beb1bacb694e9072f00851e58
uuware <uuware@gmail.com>
2025-06-06 21:09:00 +1200

    Fix HtmlVar to support changing before live

--- src/components/html-var.tsx
+++ src/components/html-var.tsx
@@ -2,32 +2,40 @@ import { mountComponents } from '../core/mount-components';
 import { RefProps, VNode } from '../jsx';
 
 export const HtmlVar = (initial?: string | VNode<any>) => {
-  const ref: RefProps = { id: '' };
+  let _value: string | VNode<any> = initial || '';
+  let _dirty = false;
+  const waitUpdate = async (value: string | VNode<any>) => {
+    if (!ref.current) return;
+    if (typeof value === 'object' && value.type && value.props) {
+      await mountComponents(ref.current, value);
+    } else {
+      ref.current.innerHTML = value;
+    }
+    _dirty = false;
+  }
+  const ref: RefProps = { onLoad: async (el: Element) => {
+    _dirty && waitUpdate(_value);
+  } };
   const FragmentRef = (props: any) => {
     return <>{props.children}</>;
   };
 
   return {
     set value(value: string | VNode<any>) {
-      this.waitUpdate(value);
-    },
-    // for any cases that needs to wait for the update to complete
-    waitUpdate: async (value: string | VNode<any>) => {
-      if (typeof value === 'object' && value.type && value.props) {
-        await mountComponents(ref.current, value);
-      } else {
-        ref.current.innerHTML = value;
-      }
+      _value = value;
+      _dirty = true;
+      waitUpdate(value);
     },
     get value() {
-      return ref.current.innerHTML;
+      return ref.current ? ref.current.innerHTML : _value;
     },
     get ref() {
       return ref;
     },
     // _fragment_ref is a special id to add ref to a fragment and it is processed in mount-components
     get node() {
-      return <FragmentRef _fragment_ref={ref}>{initial}</FragmentRef>;
+      _dirty = false;
+      return <FragmentRef _fragment_ref={ref}>{_value}</FragmentRef>;
     },
   };
 };
